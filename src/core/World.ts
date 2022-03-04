665850d2d849139df6a522f8baacaecf40ce3c95
michael5031 <48484911+michael5031@users.noreply.github.com>
2022-03-04 11:03:33 +0100

    added CubeEntity to test the physics

--- src/core/World.ts
+++ src/core/World.ts
@@ -4,6 +4,7 @@ import ElementNotFoundError from "../errors/ElementNotFoundError";
 import Updatable from "../interfaces/Updatable";
 import InputManager from "../managers/InputManager";
 import Player from "../entities/Player";
+import CubeEntity from "../entities/CubeEntity";
 
 export default class World {
   private renderer: THREE.WebGLRenderer;
@@ -14,6 +15,9 @@ export default class World {
 
   private deltaClock: THREE.Clock;
   private deltaTime: number;
+
+  private deltaPhysicsClock: THREE.Clock;
+  private deltaPhysicsTime: number;
 
   cScene: CANNON.World;
   private cSolver: CANNON.GSSolver;
@@ -30,13 +34,15 @@ export default class World {
 
   private player: Player;
 
+  private cubeEntity: CubeEntity;
+
   generateFloor() {
     this.floorGeometry = new THREE.PlaneGeometry(1000, 1000, 50, 50);
     this.floorMaterial = new THREE.MeshLambertMaterial({ color: 0xa0a0a0 });
     this.floorMesh = new THREE.Mesh(this.floorGeometry, this.floorMaterial);
     this.floorMesh.receiveShadow = true;
-    this.floorMesh.rotation.x = -Math.PI / 2;
-    this.floorMesh.position.y = -1;
+    /* this.floorMesh.rotation.x = -Math.PI / 2;
+    this.floorMesh.position.y = -1; */
     this.scene.add(this.floorMesh);
     this.camera.position.z = 5;
 
@@ -45,6 +51,12 @@ export default class World {
     this.cFloorBody = new CANNON.Body({ mass: 0 });
     this.cFloorBody.addShape(this.cFloorShape);
     this.cFloorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2);
+    this.cFloorBody.position.y = -1;
+
+    // copy valuse from physical body to mesh
+    this.floorMesh.position.copy(this.cFloorBody.position as unknown as THREE.Vector3);
+    this.floorMesh.quaternion.copy(this.cFloorBody.quaternion as unknown as THREE.Quaternion);
+
     this.cScene.addBody(this.cFloorBody);
   }
 
@@ -110,6 +122,20 @@ export default class World {
     // -- Initializes clock for delta time --
     this.deltaClock = new THREE.Clock();
     this.deltaTime = 0;
+
+    // -- Initializes clock for delta time which gets used by the physics update --
+    this.deltaPhysicsClock = new THREE.Clock();
+    this.deltaPhysicsTime = 0;
+
+    // -- Add CubeEntity to test physics --
+    for (let i = 0; i < 100; i++) {
+      let cubeEntity = new CubeEntity(new CANNON.Vec3(0, i * 10, 0));
+      cubeEntity.addToWorld(this);
+      this.updatables.push(cubeEntity);
+    }
+    /* this.cubeEntity = new CubeEntity();
+    this.cubeEntity.addToWorld(this);
+    this.updatables.push(this.cubeEntity); */
   }
 
   public handleResize(): boolean {
@@ -140,12 +166,23 @@ export default class World {
 
   public update() {
     // Update all updatables
+
+    this.player.update(this.deltaTime);
     this.updatables.forEach((updateable) => {
       updateable.update(this.deltaTime);
     });
   }
 
-  public updatePhysics() {}
+  public updatePhysics() {
+    this.deltaPhysicsTime = this.deltaPhysicsClock.getDelta();
+    this.cScene.step(1 / 60, this.deltaPhysicsTime);
+
+    this.player.updatePhysics(this.deltaPhysicsTime);
+    this.updatables.forEach((updateable) => {
+      updateable.updatePhysics(this.deltaPhysicsTime);
+    });
+
+  }
 
   public render() {
     this.handleResize();
--- src/entities/CubeEntity.ts
+++ src/entities/CubeEntity.ts
@@ -11,14 +11,18 @@ export default class CubeEntity implements Entity {
   private cShape: CANNON.Box;
   private cBody: CANNON.Body;
 
-  constructor() {
+  constructor(position: CANNON.Vec3) {
     this.geometry = new THREE.BoxGeometry();
     this.material = new THREE.MeshLambertMaterial({ color: 0xdd00dd });
     this.mesh = new THREE.Mesh(this.geometry, this.material);
+    this.mesh.castShadow = true;
+    this.mesh.receiveShadow = true;
 
-    this.cShape = new CANNON.Box(new CANNON.Vec3(1, 1, 1));
-    this.cBody = new CANNON.Body();
+    this.cShape = new CANNON.Box(new CANNON.Vec3(this.mesh.scale.x / 2, this.mesh.scale.y / 2, this.mesh.scale.z / 2));
+    this.cBody = new CANNON.Body({ mass: 5 });
+    this.cBody.position.copy(position);
     this.cBody.addShape(this.cShape);
+    this.mesh.position.copy(this.cBody.position as unknown as THREE.Vector3);
   }
 
   addToWorld(world: World): void {
@@ -30,5 +34,8 @@ export default class CubeEntity implements Entity {
     world.cScene.removeBody(this.cBody);
   }
   update(deltaTime: number): void {}
-  updatePhysics(deltaTime: number): void {}
+  updatePhysics(deltaTime: number): void {
+    this.mesh.position.copy(this.cBody.position as unknown as THREE.Vector3);
+    this.mesh.quaternion.copy(this.cBody.quaternion as unknown as THREE.Quaternion);
+  }
 }
