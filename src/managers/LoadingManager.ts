3ac6d9073dfcf43179903ec42f454dc6ba68e51f
michael5031 <48484911+michael5031@users.noreply.github.com>
2022-04-08 01:28:00 +0300

    Added A Monkey Model to test the GLTF Model Loading

--- src/managers/LoadingManager.ts
+++ src/managers/LoadingManager.ts
@@ -1,5 +1,4 @@
 import * as THREE from "three";
-import { Object3D } from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 export default class LoadingManager {
@@ -8,12 +7,15 @@ export default class LoadingManager {
     this.gltfLoader = new GLTFLoader();
   }
 
-  async loadGLTFGeometryAsync(path: string) {
+  async loadGLTFGeometryAsync(path: string): Promise<THREE.BufferGeometry> {
     try {
       let loadedGLTF = await this.gltfLoader.loadAsync(path);
-      let load = loadedGLTF.scene.children[0];
+      let load = (loadedGLTF.scene.children[0] as THREE.Mesh).geometry;
       return load;
-    } catch (err) {}
-    return new THREE.Object3D();
+    } catch (err) {
+      //TODO: add error handling if a Model isn't found
+      // return undefined;
+      return new THREE.BufferGeometry();
+    }
   }
 }
