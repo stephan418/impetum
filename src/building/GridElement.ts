2c58283a2005e728ee47b68965d46df44cb0298c
michael5031 <48484911+michael5031@users.noreply.github.com>
2022-04-08 11:38:21 +0300

    Added basic Class for Grid Element

--- src/building/GridElement.ts
+++ src/building/GridElement.ts
@@ -1,1 +1,12 @@
-// export default class
+// import World from "../core/World";
+import BaseElement from "./BaseElement";
+// import * as THREE from "three";
+
+export default abstract class GridElement extends BaseElement {
+  private direction: string;
+
+  constructor(direction: string) {
+    super();
+    this.direction = direction;
+  }
+}
