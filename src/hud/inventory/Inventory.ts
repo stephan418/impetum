231d0868f59358477836d598478e1aa0e07a8892
Stephan <57194608+stephan418@users.noreply.github.com>
2022-05-12 13:21:01 +0200

    Style the inventory grid

--- src/hud/inventory/Inventory.ts
+++ src/hud/inventory/Inventory.ts
@@ -1,9 +1,28 @@
-import { html, LitElement } from "lit";
-import { customElement, property } from "lit/decorators";
+import { css, html, LitElement } from "lit";
+import { customElement, property } from "lit/decorators.js";
 import PlayerInventory from "../../inventory/PlayerInventory";
 
 @customElement("i-inventory")
 export class Inventory extends LitElement {
+  static styles = css`
+    :host {
+      display: block;
+      position: relative;
+      width: 100%;
+      max-width: 100%;
+      margin-bottom: 10%;
+
+      justify-content: center;
+
+      display: grid;
+
+      grid-template-columns: repeat(auto-fill, 50px);
+      grid-template-rows: repeat(auto-fill, 50px);
+
+      gap: 10px;
+    }
+  `;
+
   @property()
   inventory?: PlayerInventory;
 
