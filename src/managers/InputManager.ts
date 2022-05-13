4905ad37a768be08a0dcb4b723f92ec600ad0b42
michael5031 <48484911+michael5031@users.noreply.github.com>
2022-05-13 10:51:18 +0200

    Added mouse buttons for the Input Manager

--- src/managers/InputManager.ts
+++ src/managers/InputManager.ts
@@ -2,12 +2,21 @@ export default class InputManager {
   private pressedKeys: Map<string, boolean>;
   private keysCallbacks: Map<string, Function>;
 
+  private pressedMouseButtons: Map<number, boolean>;
+  private mouseCallbacks: Map<number, Function>;
+
   constructor() {
     this.pressedKeys = new Map();
     this.keysCallbacks = new Map();
 
+    this.pressedMouseButtons = new Map();
+    this.mouseCallbacks = new Map();
+
     document.addEventListener("keydown", this.onKeyDown.bind(this));
     document.addEventListener("keyup", this.onKeyUp.bind(this));
+
+    document.addEventListener("mousedown", this.onMouseDown.bind(this));
+    document.addEventListener("mouseup", this.onMouseUp.bind(this));
   }
 
   private onKeyDown(ev: KeyboardEvent) {
@@ -37,5 +46,27 @@ export default class InputManager {
   removeKeyCallback(key: string) {
     if (this.keysCallbacks.get(key) == undefined) return;
     this.keysCallbacks.delete(key);
+  }
+
+  private onMouseDown(ev: MouseEvent) {
+    this.pressedMouseButtons.set(ev.which, true);
+    if (this.mouseCallbacks.get(ev.which) == undefined) return;
+    this.mouseCallbacks.get(ev.which)?.(true);
+  }
+  private onMouseUp(ev: MouseEvent) {
+    this.pressedMouseButtons.set(ev.which, false);
+    if (this.mouseCallbacks.get(ev.which) == undefined) return;
+    this.mouseCallbacks.get(ev.which)?.(false);
+  }
+  isClicked(which: number) {
+    let result = this.pressedMouseButtons.get(which);
+    if (result != undefined) {
+      return this.pressedMouseButtons.get(which);
+    }
+    return false;
+  }
+
+  addMouseButtonCallback(which: number, callback: (pressed: boolean) => void) {
+    this.mouseCallbacks.set(which, callback);
   }
 }
