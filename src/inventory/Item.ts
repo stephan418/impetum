22b945a1a9602a19f98c6281bd7e7a0d3af56a52
Stephan <57194608+stephan418@users.noreply.github.com>
2022-06-02 09:51:08 +0000

    Add icons to Items
    + Add icon support to InventorySlot

--- src/inventory/Item.ts
+++ src/inventory/Item.ts
@@ -2,6 +2,8 @@ import IItem from "../interfaces/Item";
 
 export default abstract class Item implements IItem {
   public static readonly _id: string;
+  public static readonly _icon?: string;
 
   abstract get id(): string;
+  abstract get icon(): string | undefined;
 }
