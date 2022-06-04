7af0e455a31daac7c0fe183ccda7ace80b38bbbe
michael5031 <48484911+michael5031@users.noreply.github.com>
2022-06-04 16:32:06 +0200

    Rewrote the movement, fixed the not being able to move after vel=0 bug, added contact materials

--- src/building/WallElement.ts
+++ src/building/WallElement.ts
@@ -1,9 +1,11 @@
 import GridElement from "./GridElement";
 import * as THREE from "three";
 import * as CANNON from "cannon-es";
+import { slipperyMaterial } from "../core/CannonMaterials";
 
 export default abstract class WallElement extends GridElement {
   constructor(geometry: THREE.BufferGeometry, material: THREE.Material, cShape: CANNON.Shape) {
     super(geometry, material, cShape);
+    this.getCBody().material = slipperyMaterial;
   }
 }
---
+++ src/core/CannonMaterials.ts
@@ -0,0 +1,3 @@
+import * as CANNON from "cannon-es";
+export const groundMaterial = new CANNON.Material("ground");
+export const slipperyMaterial = new CANNON.Material("slippery");
