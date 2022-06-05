a40a781cd400e8759f4f06c9f88668c88056eb36
michael5031 <48484911+michael5031@users.noreply.github.com>
2022-06-05 16:38:10 +0200

    Rewrote FreeElement to allow multiple meshes and bodies

--- src/building/TurretElement.ts
+++ src/building/TurretElement.ts
@@ -3,10 +3,14 @@ import * as THREE from "three";
 import * as CANNON from "cannon-es";
 import { groundMaterial, slipperyMaterial } from "../core/CannonMaterials";
 import FreeElement from "./FreeElement";
+import { ShapeAndOffset } from "../managers/ResourceManager";
 
 export default abstract class TurretElement extends FreeElement {
-  constructor(geometry: THREE.BufferGeometry, material: THREE.Material, cShape: CANNON.Shape) {
-    super(geometry, material, cShape);
-    this.getCBody().material = groundMaterial;
+  constructor(
+    geometries: THREE.BufferGeometry[] | THREE.BufferGeometry,
+    materials: THREE.Material[] | THREE.Material,
+    cShapes: ShapeAndOffset[][] | ShapeAndOffset[] | ShapeAndOffset
+  ) {
+    super(geometries, materials, cShapes);
   }
 }
