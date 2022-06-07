86c9a0f64ce6e0eeb07e48b6ef891d15321d187c
michael5031 <48484911+michael5031@users.noreply.github.com>
2022-06-07 16:58:31 +0200

    Added basic icons for woodenWall, woodenFloor and generalTurret

--- src/inventory/items/GeneralTurretItem.ts
+++ src/inventory/items/GeneralTurretItem.ts
@@ -14,6 +14,6 @@ export default class GeneralTurretItem extends Item implements Buildable<typeof 
   }
 
   get icon() {
-    return undefined;
+    return "../../../static/items/generalTurretItem.png";
   }
 }
