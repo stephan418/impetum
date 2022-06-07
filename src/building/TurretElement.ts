d70928e1056447ae36845014f9d8a1835a78a3eb
michael5031 <48484911+michael5031@users.noreply.github.com>
2022-06-07 14:30:35 +0200

    Added turret rotation logic

--- src/building/TurretElement.ts
+++ src/building/TurretElement.ts
@@ -7,27 +7,28 @@ import { ShapeAndOffset } from "../managers/ResourceManager";
 import Updatable from "../interfaces/Updatable";
 
 export default abstract class TurretElement extends FreeElement implements Updatable {
-  private turretRotationY: number;
   constructor(
     geometries: THREE.BufferGeometry[] | THREE.BufferGeometry,
     materials: THREE.Material[] | THREE.Material,
     cShapes: ShapeAndOffset[][] | ShapeAndOffset[] | ShapeAndOffset
   ) {
     super(geometries, materials, cShapes);
-    this.getParts()[1].positionOffset = new THREE.Vector3(0, 1, 0);
-    this.getParts()[2].positionOffset = new THREE.Vector3(0, 2, 0);
-    this.turretRotationY = 0;
   }
   update(deltaTime: number): void {
-    this.turretRotationY += deltaTime;
-    let tempQuat = new THREE.Quaternion();
-    tempQuat.setFromEuler(new THREE.Euler(0, this.turretRotationY / 2, 0));
-    this.getParts()[1].quaternionOffset = tempQuat.clone();
-    this.getParts()[1].mesh.add(this.getParts()[2].mesh);
-    this.getParts()[2].position = new THREE.Vector3();
-    this.getParts()[2].quaternionOffset = new THREE.Quaternion().setFromEuler(
-      new THREE.Euler(Math.cos(this.turretRotationY) / 2, 0, 0)
-    );
+    this._update(deltaTime);
   }
-  updatePhysics(deltaTime: number): void {}
+  updatePhysics(deltaTime: number): void {
+    this._updatePhysics(deltaTime);
+  }
+
+  public lookAt(pos: THREE.Vector3) {
+    this._lookAt(pos);
+  }
+  public shoot() {
+    this._shoot();
+  }
+  protected abstract _lookAt(pos: THREE.Vector3): void;
+  protected abstract _shoot(): void;
+  protected abstract _update(deltaTime: number): void;
+  protected abstract _updatePhysics(deltaTime: number): void;
 }
