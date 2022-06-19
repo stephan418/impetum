6f97ebace07195b48134a4cbd1e891f80be27d72
Stephan <57194608+stephan418@users.noreply.github.com>
2022-06-19 19:52:26 +0000

    Change code so that inventory savestates are loaded

---
+++ src/managers/StorageManagers.ts
@@ -0,0 +1,21 @@
+import PlayerInventory from "../inventory/PlayerInventory";
+
+export default class StorageManager {
+  private inventory?: PlayerInventory;
+
+  getOrInitInventory(...args: ConstructorParameters<typeof PlayerInventory>) {
+    const serialized = localStorage.getItem("i-store.inventory");
+
+    if (!serialized) {
+      this.inventory = new PlayerInventory(...args);
+      return this.inventory;
+    }
+
+    this.inventory = PlayerInventory.fromSerialized(serialized, args[0], args[1], args[2]);
+    return this.inventory;
+  }
+
+  saveAll() {
+    if (this.inventory) localStorage.setItem("i-store.inventory", this.inventory.serialize());
+  }
+}
